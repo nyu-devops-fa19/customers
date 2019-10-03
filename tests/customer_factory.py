ca208e6695bf98867033546c7a35a68777326569
Tianpei Wang <tianpei3@gmail.com>
2019-10-03 15:23:04 -0400

    update

--- tests/customer_factory.py
+++ tests/customer_factory.py
@@ -17,18 +17,8 @@ Test Factory to make fake objects for testing
 """
 import factory
 from factory.fuzzy import FuzzyChoice
-from service.models import Pet
+from service.models import Customer
 
-class PetFactory(factory.Factory):
-    """ Creates fake pets that you don't have to feed """
-    class Meta:
-        model = Pet
-    id = factory.Sequence(lambda n: n)
-    name = factory.Faker('first_name')
-    category = FuzzyChoice(choices=['dog', 'cat', 'bird', 'fish'])
-    available = FuzzyChoice(choices=[True, False])
-
-if __name__ == '__main__':
-    for _ in range(10):
-        pet = PetFactory()
-        print(pet.serialize())
+class CustomerFactory(factory.Factory):
+    """ Creates fake customers that you don't have to feed """
+    
