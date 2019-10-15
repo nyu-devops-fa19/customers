64124fdd3520574de1ff90703ecef9886e37b2e6
Tianpei Wang <tianpei3@gmail.com>
2019-10-15 18:01:33 -0400

    factory

--- tests/customer_factory.py
+++ tests/customer_factory.py
@@ -19,31 +19,32 @@ import factory
 import string
 import random
 from factory.fuzzy import FuzzyChoice, FuzzyInteger, FuzzyText
-from service.models import Customer
+from service.models import Customer, Address
 
 class AddressFactory(factory.Factory):
-    id = factory.Sequence(lambda n: n)
-
-    street = FuzzyText(random.randint(1,20), chars=string.ascii_letters + string.digits)
-    apartment = FuzzyText(random.randint(1,20), chars=string.ascii_letters + string.digits)
-    city = FuzzyText(random.randint(1,20), chars=string.ascii_letters + string.digits)
-    state = FuzzyText(random.randint(1,20), chars=string.ascii_letters + string.digits)
-    zip_code = FuzzyText(5, chars=string.digits)
-    customer_id = factory.Sequence(lambda n: n)
-
+	""" Creates fake addresses that you don't have to feed """
+	class Meta:
+		model = Address
+	id = factory.Sequence(lambda n: n)
+	street = FuzzyText(random.randint(1,20))
+	apartment = FuzzyText(random.randint(1,20))
+	city = FuzzyText(random.randint(1,20))
+	state = FuzzyText(random.randint(1,20))
+	zip_code = FuzzyText(5, chars=string.digits)
+	customer_id = factory.Sequence(lambda n: n)
 
 
 class CustomerFactory(factory.Factory):
     """ Creates fake customers that you don't have to feed """
     class Meta:
         model = Customer
+    customer_id = factory.Sequence(lambda n: n)
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
-    Address = AddressFactory()
-
-    user_id = FuzzyText(random.randint(1,20), chars=string.ascii_letters + string.digits) # Assume that the length of user_id is 8
-    password = FuzzyText(random.randint(1,20), chars=string.ascii_letters + string.digits) # Assume that the length of password is 8
+    user_id = FuzzyText(random.randint(1,20))
+    password = FuzzyText(random.randint(1,20))
     active = FuzzyChoice(choices=[True, False])
+    address_id = factory.Sequence(lambda n: n)
 
 if __name__ == '__main__':
     for _ in range(10):
